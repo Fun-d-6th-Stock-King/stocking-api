364746b132a199b3a71395f9b87c0211c32fd9f9
dchecheb <qocodbssla@naver.com>
2021-04-02 20:41:32 +0900

    feat: OldStock JPA 리포지터리 추가 #13

---
+++ src/main/java/com/stocking/modules/buythen/OldStockRepository.java
@@ -0,0 +1,7 @@
+package com.stocking.modules.buythen;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OldStockRepository extends JpaRepository<OldStock, Long> {
+}
