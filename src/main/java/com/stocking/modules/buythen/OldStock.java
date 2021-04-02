3575965221241d7a5b5464cb589daa503e20df65
dchecheb <qocodbssla@naver.com>
2021-04-02 20:40:16 +0900

    feat: OldStock 개체 추가 과거 주가 DB 매핑용 객체 추가 #13

---
+++ src/main/java/com/stocking/modules/buythen/OldStock.java
@@ -0,0 +1,43 @@
+package com.stocking.modules.buythen;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Entity
+public class OldStock {
+
+    @Id
+    @ApiModelProperty(notes = "ID")
+    private Long id;
+
+    @Column
+    @ApiModelProperty(notes = "회사명", example = "삼성전자")
+    private String company;
+
+    @Column
+    @ApiModelProperty(notes = "종목코드")
+    private String code;
+
+    @Column
+    @ApiModelProperty(notes = "1년 전 주가")
+    private Long oneAgoStock;
+
+    @Column
+    @ApiModelProperty(notes = "5년 전 주가")
+    private Long fiveAgoStock;
+
+    @Column
+    @ApiModelProperty(notes = "10년 전 주가")
+    private Long tenAgoStock;
+
+    @Column
+    @ApiModelProperty(notes = "현재 주가")
+    private Long currentStock;
+
+    @Column
+    @ApiModelProperty(notes = "stock ID")
+    private Long stockId;
+}
