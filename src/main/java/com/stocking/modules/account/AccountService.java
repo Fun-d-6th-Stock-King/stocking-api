135c5380f0a976c32eb36502f0f6b7616e39c994
JJungwoo <jungwoo5759@gmail.com>
2021-04-13 22:40:53 +0900

    feat: 최대 상승 주식 목록 조회 기능 구현

--- src/main/java/com/stocking/modules/account/AccountService.java
+++ src/main/java/com/stocking/modules/account/AccountService.java
@@ -1,22 +1,45 @@
 package com.stocking.modules.account;
 
-import com.stocking.modules.buythen.repo.StocksPriceRepository;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.stocking.modules.buythen.repo.QStocksPrice;
+import com.stocking.modules.stock.QStock;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 public class AccountService {
 
     private final AccountRepository accountRepository;
-    private final StocksPriceRepository stocksPriceRepository;
+
+    private final JPAQueryFactory queryFactory;
 
     List<String> getBestStocks() {
-        List<String> list = null;
-        return list;
+
+        QStocksPrice qStocksPrice = QStocksPrice.stocksPrice;
+        QStock qStock = QStock.stock;
+
+        List<Long> bestStockIdList = queryFactory.select(qStocksPrice.stocksId)
+                .from(qStocksPrice)
+                .where(qStocksPrice.marketCap.isNotNull())
+                .orderBy(qStocksPrice.marketCap.desc())
+                .limit(9)
+                .fetch();
+
+        List<String> bestStockNameList = bestStockIdList.stream()
+                .map(vo -> {
+                    String company = queryFactory.select(qStock.company)
+                            .from(qStock)
+                            .where(qStock.id.eq(vo))
+                            .fetchOne();
+                    return company;
+                }).collect(Collectors.toList());
+
+        return bestStockNameList;
     }
 
     public void saveNewAccount(SignUpForm signUpForm) {
