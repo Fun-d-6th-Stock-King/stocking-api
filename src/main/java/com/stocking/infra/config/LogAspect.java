0ab60d08ae700406494f3c25c2e59447cadc9f88
박기현 <keehyun2@gmail.com>
2021-05-02 10:46:57 +0900

    feat: aop 로그 적용중

--- src/main/java/com/stocking/infra/config/LogAspect.java
+++ src/main/java/com/stocking/infra/config/LogAspect.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletRequest;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -18,26 +20,29 @@ import com.google.common.base.Joiner;
 import lombok.extern.slf4j.Slf4j;
 
 @Aspect
+@Order(Ordered.HIGHEST_PRECEDENCE)
 @Component
 @Slf4j
 public class LogAspect {
 
-    @Around("within(com.stocking.modules..*)")
+    @Around("(within(@org.springframework.stereotype.Controller *)"
+            + "|| within(@org.springframework.web.bind.annotation.RestController *))"
+            + "&& execution(public * *(..))")
     public Object logging(ProceedingJoinPoint pjp) throws Throwable {
 
         String params = getRequestParams();
 
         long startAt = System.currentTimeMillis();
 
-        log.info("----------> REQUEST : {}({}) = {}", pjp.getSignature().getDeclaringTypeName(),
+        log.info("==================== REQUEST : {}({}) = {}", pjp.getSignature().getDeclaringTypeName(),
                 pjp.getSignature().getName(), params);
 
         Object result = pjp.proceed();
 
         long endAt = System.currentTimeMillis();
 
-        log.info("----------> RESPONSE : {}({}) = {} ({}ms)", pjp.getSignature().getDeclaringTypeName(),
-                pjp.getSignature().getName(), result, endAt-startAt);
+        log.info("==================== RESPONSE : {}({}) = ({}ms)", pjp.getSignature().getDeclaringTypeName(),
+                pjp.getSignature().getName(), endAt-startAt);
 
         return result;
 
@@ -51,8 +56,7 @@ public class LogAspect {
         RequestAttributes requestAttribute = RequestContextHolder.getRequestAttributes();
 
         if(requestAttribute != null){
-            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder
-                    .getRequestAttributes()).getRequest();
+            HttpServletRequest request = ((ServletRequestAttributes) requestAttribute).getRequest();
 
             Map<String, String[]> paramMap = request.getParameterMap();
 
