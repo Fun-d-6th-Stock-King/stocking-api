de6f184d488c6e7fee468f1e9f5efafd9239bb8d
박기현 <keehyun2@gmail.com>
2021-05-29 12:30:08 +0900

    작업중

---
+++ src/main/java/com/stocking/modules/buythen/CalcAllRes.java
@@ -0,0 +1,79 @@
+package com.stocking.modules.buythen;
+
+import java.math.BigDecimal;
+
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+@Builder
+public class CalcAllRes {
+
+    @ApiModelProperty(notes = "종목 코드", required=false, position=1)
+    private String code;    
+    @ApiModelProperty(notes = "회사명", required=false, position=2)
+    private String company; 
+    @ApiModelProperty(notes = "현재 주가", required=false, position=3)
+    private BigDecimal currentPrice; 
+    @ApiModelProperty(notes = "마지막 거래 일시", required=false, position=4)
+    private String lastTradingDateTime;
+    
+    @ApiModelProperty(notes = "계산 결과", required=false, position=5)
+    private CalculatedValue calculatedValue;
+    @ApiModelProperty(notes = "예외 케이스", required=false, position=6)
+    private ExceptionCase exceptionCase;
+    
+    @Data
+    @AllArgsConstructor
+    @Builder
+    public static class CalculatedValue {
+        @ApiModelProperty(notes = "투자시기", required=false, position=1)
+        private String investDate;
+        @ApiModelProperty(notes = "투자금", required=false, position=2)
+        private BigDecimal investPrice;
+        @ApiModelProperty(notes = "수익금", required=false, position=3)
+        private BigDecimal yieldPrice;
+        @ApiModelProperty(notes = "수익률", required=false, position=4)
+        private BigDecimal yieldPercent;
+        @ApiModelProperty(notes = "종가일자", required=false, position=5)
+        private String oldCloseDate;
+        @ApiModelProperty(notes = "그때 주가", required=false, position=6)
+        private BigDecimal oldPrice;
+        @ApiModelProperty(notes = "보유 주식 수", required=false, position=7)
+        private BigDecimal holdingStock;
+        @ApiModelProperty(notes = "연봉", required=false, position=8)
+        private BigDecimal salaryYear;
+        @ApiModelProperty(notes = "월급", required=false, position=9)
+        private BigDecimal salaryMonth;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @Builder
+    public static class ExceptionCase {
+        @ApiModelProperty(notes = "예외 케이스 여부", required=false, position=1)
+        private Boolean isExceptionCase;
+
+        @ApiModelProperty(notes = "기간값 예외 케이스 여부", required=false, position=2)
+        private Boolean isDateException;
+        @ApiModelProperty(notes = "기존 투자 날짜", required=false, position=3)
+        private InvestDate oldInvestDate;
+        @ApiModelProperty(notes = "새 투자 날짜", required=false, position=4)
+        private InvestDate newInvestDate;
+
+        @ApiModelProperty(notes = "금액값 예외 케이스 여부", required=false, position=5)
+        private Boolean isPriceException;
+        @ApiModelProperty(notes = "기존 금액값", required=false, position=6)
+        private BigDecimal oldInvestPrice;
+        @ApiModelProperty(notes = "새 금액값", required=false, position=7)
+        private BigDecimal newInvestPrice;
+
+        @ApiModelProperty(notes = "종목 예외 케이스 여부", required=false, position=8)
+        private Boolean isStockException;
+        @ApiModelProperty(notes = "종목 예외 경고 메시지", required=false, position=9)
+        private String stockWarnMsg;
+    }
+}
